e32626cb74309125511ab233e47cbfe38f7feee4
dika aditia <aditiaaadicka@gmail.com>
2020-07-14 08:25:23 +0700

    create logo as parts

--- src/elements/Button/index.test.js
+++ src/elements/Button/index.test.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Button from "./index";
@@ -6,4 +7,24 @@ import Button from "./index";
 test("should not allowed click button if isDisabled is present", () => {
   const { container } = render(<Button isDisabled></Button>);
   expect(container.querySelector("span.isDisabled")).toBeInTheDocument;
+});
+
+test("should render loading/spinner", () => {
+  const { container, getByText } = render(<Button isLoading></Button>);
+  expect(getByText(/loading/i)).toBeInTheDocument;
+  expect(container.querySelector("span")).toBeInTheDocument;
+});
+
+test("should render <a></a> tag", () => {
+  const { container } = render(<Button type="link" isExternal></Button>);
+  expect(container.querySelector("a")).toBeInTheDocument;
+});
+
+test("should render <Link> tag", () => {
+  const { container } = render(
+    <Router>
+      <Button href="" type="link"></Button>
+    </Router>
+  );
+  expect(container.querySelector("a")).toBeInTheDocument;
 });
