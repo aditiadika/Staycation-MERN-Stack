533357893273d5fc5d55bf96fe1a86a54e950c84
dika aditia <aditiaaadicka@gmail.com>
2020-07-15 08:35:36 +0700

    wip on landing page

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "parts/Header";
+
+export default function LandingPage() {
+  return (
+    <React.Fragment>
+      <Header {...this.props} />
+    </React.Fragment>
+  );
+}
