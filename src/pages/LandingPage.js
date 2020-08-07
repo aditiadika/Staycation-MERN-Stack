e01963c190e2b433a9853d5ef0a61997217b8152
dika aditia <aditiaaadicka@gmail.com>
2020-08-07 09:07:37 +0700

    add image and icon hero, on hero page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "parts/Header";
 
+import DataLanding from "json/landingPage.json";
+import Hero from "parts/Hero";
+
 export default function LandingPage(props) {
   return (
     <React.Fragment>
       <Header {...props} />
+      <Hero data={DataLanding.hero} />
     </React.Fragment>
   );
 }
---
+++ src/parts/Hero.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import HeroFrame from "assets/images/hero-frame.jpg";
+import HeroPicture from "assets/images/hero-picture.jpg";
+import IconCities from "assets/images/icon/ic_cities.svg";
+import IconTraveller from "assets/images/icon/ic_traveler.svg";
+import IconTreasure from "assets/images/icon/ic_treasure.svg";
+
+import Button from "elements/Button";
+
+export default function Hero(props) {
+  showMostPicked = () => {
+    window.scrollTo();
+  };
+
+  return (
+    <section className="container pt-4">
+      <div className="row align-items-center">
+        <div className="col-auto pr-5" style={{ width: 422 }}>
+          <h1 className="h2 font-weight-bold line-height-1 mb-3">
+            Forget Busy Work, <br /> Start Next Vacation
+          </h1>
+          <p className="mb-5 font-weight-light text-gray-500 w-75">
+            We provide what you need to enjoy your holiday with family. Time to
+            make another memorable moments.
+          </p>
+          <Button
+            className="btn px-5"
+            hasShadow
+            isPrimary
+            onClick={showMostPicked}
+          >
+            Show Me Now
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+}
